aebc1b220e7f1e8a16cce8e688466659ef18874b
Wes Buck <wesbuck@gmail.com>
2020-01-29 21:18:48 -0500

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,12 @@ module.exports = {
                 pageContextProperty: `menus`,
                 menus: require('./src/data/menus.json'),
             }
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+              trackingId: 'UA-10631001-1'
+            }
         }
     ]
 };
